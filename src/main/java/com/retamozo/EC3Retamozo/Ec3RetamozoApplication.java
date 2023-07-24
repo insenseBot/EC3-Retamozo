91856adfddce888f45722f416c8e3a278c97e613
insenseBot <129139229+insenseBot@users.noreply.github.com>
2023-07-24 17:47:45 +0000

    Prueba 1

---
+++ src/main/java/com/retamozo/EC3Retamozo/Ec3RetamozoApplication.java
@@ -0,0 +1,12 @@
+package com.retamozo.EC3Retamozo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3RetamozoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3RetamozoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/retamozo/EC3Retamozo/Ec3RetamozoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retamozo.EC3Retamozo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3RetamozoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
